3d628f11dab3cee925b90d300e7ba5e28f1ef442
Diego Flores Pérez <diegofloresperez90@gmail.com>
2017-05-16 10:13:51 +0200

    travis

--- test_calculadora.py
+++ test_calculadora.py
@@ -2,8 +2,8 @@ from calculadora import calculadora
 
 def test_suma():
     calc = calculadora()
-    resultado = calc.suma(2, 2)
-    assert resultado == 4
+    resultado = calc.suma(2, 3)
+    assert resultado == 5
 
 def test_resta():
     calc = calculadora()
